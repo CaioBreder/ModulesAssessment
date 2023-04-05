2ab4213c42ae9721c5ceef49f401543bad7c3679
Caio Breder <cbernardodelima@ucf.edu>
2023-04-05 04:48:35 -0400

    ModulesAssessment

---
+++ module1.js
@@ -0,0 +1,17 @@
+let var1 = 'Hi';
+let var2 = 'My name is Caio';
+let var3 = 42;
+
+function func1() {
+	console.log('Function 1');
+}
+
+function func2() {
+	console.log('Function 2');
+}
+
+function func3() {
+	console.log('Function 3');
+}
+
+export { var1, var2, var3, func1, func2, func3 };
---
+++ module2.js
@@ -0,0 +1,9 @@
+import * as module1 from './module1.js';
+
+console.log(module1.var1); 
+console.log(module1.var2); 
+console.log(module1.var3); 
+
+module1.func1(); 
+module1.func2(); 
+module1.func3(); 
